adc53a065bfdefe889824dd0c46ad45c8ad72b1f
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-09-01 19:46:50 -0500

    functioning with name, age, sex, size, description

--- app.rb
+++ app.rb
@@ -2,14 +2,18 @@ require 'bundler'
 Bundler.require()
 
 get '/' do
-	xml_to_json_to_firebase
+	petfinder_to_firebase
 end
 
 
-def xml_to_json_to_firebase()
+def petfinder_to_firebase()
 	# get petfinder xml
-	@xml = HTTParty.get(ENV['PETFINDER_URI'])
+	@xml = HTTParty.get(ENV['PETFINDER_URI'])	
+	firebase = Firebase::Client.new(ENV['FIREBASE_URI'])
 
+	# for now, just wiping and refilling with all data
+	response = firebase.delete("pets")
+	
 	# convert any breed Strings to Arrays
 	@xml["petfinder"]["pets"].each do |pet|
 		i = 0
@@ -17,17 +21,24 @@ def xml_to_json_to_firebase()
 
 		while i < length
 			breed = pet[1][i]["breeds"]["breed"]
-			if breed.class == Strings
+			if breed.class == String
 				arr = Array.new
 				arr.push(breed)
-				pet[1][i]["breeds"]["breed"] = arr
+				pet[1][i]["breeds"]["breed"] = arr	
 			end
+			
+			response = firebase.push("pets", {
+					:name => pet[1][i]["name"],
+					:size => pet[1][i]["size"],
+					:age => pet[1][i]["age"],
+					:sex => pet[1][i]["sex"],	
+					:description => pet[1][i]["description"]
+					})
 			i += 1
 		end
 	end
 
-	#firebase = Firebase::Client.new(ENV['FIREBASE_URI'], ENV['FIREBASE_KEY'])
-	firebase = Firebase::Client.new(ENV['FIREBASE_URI'])
-	response = firebase.push('', @xml.to_json)
+	# something to send to the screen to make the web server happy
+	@xml.to_json
 
 end
